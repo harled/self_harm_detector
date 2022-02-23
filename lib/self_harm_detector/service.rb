80b461bb822802dc3e02dcb886a93aaa4de9e5d1
Jonathan Loos <jonathanloos97@gmail.com>
2022-02-23 00:02:21 -0500

    Initial gem functionality

---
+++ lib/self_harm_detector/service.rb
@@ -0,0 +1,58 @@
+module SelfHarmDetector
+    class Service
+        COMMON_SELF_HARM_PHRASES = {
+            "I want to hurt myself": {
+                category: :dysfunctional_thoughts
+            },
+            "I can’t bear to lie awake with my thoughts": {
+                category: :dysfunctional_thoughts
+            },
+            "I hate myself": {
+                category: :dysfunctional_thoughts
+            },
+            "I’m a worthless and disgusting trash excuse for a human": {
+                category: :dysfunctional_thoughts
+            },
+            "I am a worthless human being": {
+                category: :dysfunctional_thoughts
+            },
+            "I am worthless": {
+                category: :dysfunctional_thoughts
+            },
+            "I am disgusting": {
+                category: :dysfunctional_thoughts
+            },
+            "everyone hates me, even my friends/family/partner": {
+                category: :dysfunctional_thoughts
+            },
+            "everyone hates me, even my friends": {
+                category: :dysfunctional_thoughts
+            },
+            "everyone hates me, even my family": {
+                category: :dysfunctional_thoughts
+            },
+            "everyone hates me, even my partner": {
+                category: :dysfunctional_thoughts
+            },
+            "everyone hates me": {
+                category: :dysfunctional_thoughts
+            },
+            "Demons are pulling me under": {
+                category: :dysfunctional_thoughts
+            },
+            "just give up, no one loves you": {
+                category: :dysfunctional_thoughts
+            },
+            "I want to give up": {
+                category: :dysfunctional_thoughts
+            },
+            "no one loves me": {
+                category: :dysfunctional_thoughts
+            },
+            "I have no other escape": {
+                category: :dysfunctional_thoughts
+            },
+
+        }
+    end
+end
