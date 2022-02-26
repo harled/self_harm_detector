0d56662bfca5eec54c14b480ac9d2a218974133d
Jonathan Loos <jonathanloos97@gmail.com>
2022-02-25 22:26:53 -0500

    Add comments

--- lib/self_harm_detector.rb
+++ lib/self_harm_detector.rb
@@ -6,10 +6,21 @@ require_relative "self_harm_detector/sensitive_content/data"
 #   class Post
 #     include SelfHarmDetector
 #   end
+#   self_harm_matches = post.detect_self_harm(text: post.description)
+#
+#   Expects a text to be a string
 module SelfHarmDetector
+  # TODO: Add ActiveRecord support
+  # TODO: Add attribute to call to automatically score it on save with `attribute_name_self_harm_score`
+  # TODO: Add rubocop 
+  # TODO: Add rspec tests
+  # TODO: Add build and test badges to readme
+  # TODO: Add example use case case to readme
+  # TODO: Add usage to readme
   def self.included(base)
     def detect_self_harm(text:)
       SelfHarmDetector::SensitiveContent::Data::COMMON_SELF_HARM_PHRASES.keys.filter{ |phrase| 
+          # TODO: Remove symbols and numbers
           text.downcase.scan(/(?=#{phrase.to_s.downcase})/).count > 0
       }.count
     end
