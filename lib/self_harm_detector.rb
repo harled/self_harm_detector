5e88a1cfecacca4f54869567ac82e6454bb2db57
Jonathan Loos <jonathanloos97@gmail.com>
2022-02-25 21:58:32 -0500

    Add first iteration of detector

--- lib/self_harm_detector.rb
+++ lib/self_harm_detector.rb
@@ -1,8 +1,12 @@
-# frozen_string_literal: true
-
 require_relative "self_harm_detector/version"
+require_relative "self_harm_detector/sensitive_content/data"
 
 module SelfHarmDetector
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.included(base)
+    def detect_self_harm(text:)
+      SelfHarmDetector::SensitiveContent::Data::COMMON_SELF_HARM_PHRASES.keys.filter{ |phrase| 
+          text.downcase.scan(/(?=#{phrase.to_s.downcase})/).count > 0
+      }.count
+    end
+  end
 end
