d3334851f1133261c6e4b1252b8eb80e0988559b
Jonathan Loos <jonathanloos97@gmail.com>
2022-02-25 22:15:20 -0500

    Add comments:

--- lib/self_harm_detector.rb
+++ lib/self_harm_detector.rb
@@ -1,6 +1,11 @@
 require_relative "self_harm_detector/version"
 require_relative "self_harm_detector/sensitive_content/data"
 
+# Specify this act to access the detect_self_harm method. Returns
+# a count of direct matches to the inputted string.
+#   class Post
+#     include SelfHarmDetector
+#   end
 module SelfHarmDetector
   def self.included(base)
     def detect_self_harm(text:)
